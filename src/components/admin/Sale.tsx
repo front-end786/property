7581c02ada8cd6e3010da6ae8e8df3c13e4aeefb
Asad Ullah <developerfrontend786@gmail.com>
2024-08-03 23:28:52 +0500

    Just Style changes

--- src/components/admin/Sale.tsx
+++ src/components/admin/Sale.tsx
@@ -2,62 +2,64 @@ import React, { useId, useState } from "react";
 import { FeeTable } from "./sub_Components/feetable";
 
 function Sale() {
-  const [addCrud, setAddcrud] = useState([]);
-  const [crudName, setCrudName] = useState("");
-  const crudListId = useId;
-
   const crudObjectArr = [
     {
       comp: <FeeTable />,
-      id: crudListId,
     },
     {
       comp: <FeeTable />,
-      id: crudListId,
     },
     {
       comp: <FeeTable />,
-      id: crudListId,
     },
     {
       comp: <FeeTable />,
-      id: crudListId,
     },
     {
       comp: <FeeTable />,
-      id: crudListId,
     },
     {
       comp: <FeeTable />,
-      id: crudListId,
     },
     {
       comp: <FeeTable />,
-      id: crudListId,
     },
     {
       comp: <FeeTable />,
-      id: crudListId,
     },
     {
       comp: <FeeTable />,
-      id: crudListId,
     },
   ];
-  const handleAdd = () => {};
-  const deleteCrudList = () => {
-    setAddcrud(addCrud.filter((item) => item.id !== crudListId));
+  const [crudObject, setCrudObject] = useState(crudObjectArr);
+  // handleAdd function
+  const handleAdd = () => {
+    const newCrudObject = {
+      comp: <FeeTable />,
+    };
+    console.log("hello wolds");
+    setCrudObject([...crudObject, newCrudObject]);
   };
   return (
     <div className="row">
       <div>
-        <div className="headtable">
-          <p>Property Value</p>
-          <div>
-            <p>Leagel Fees</p>
-            <p>Percentage of Value</p>
-            <p>Plused Fixed Fee</p>
-            <p>Priced on Application</p>
+        <div className="flex justify-between">
+          <p className="text-center w-[50%] font-bold text-gray-900 text-xl">
+            Property Value
+          </p>
+          <div className="flex w-[50%] gap-14">
+            <p className="w-80 font-bold text-gray-900 text-xl text-center">
+              Leagel Fees
+            </p>
+            <p className="font-bold text-gray-900 text-xl text-center">
+              Percentage of Value
+            </p>
+            <p className=" font-bold text-gray-900 text-xl text-center">
+              Plused Fixed Fee
+            </p>
+            <p className="font-bold text-gray-900 text-xl text-center">
+              Priced on Application
+            </p>
           </div>
         </div>
         <div className="datatable">
@@ -65,14 +67,19 @@ function Sale() {
             <input type="text" placeholder="0" />
             <input type="text" placeholder="150,000" />
           </div>
-          <div className="valuesdata">
-            <div>
+          <div className="valuesdata gap-14">
+            <div className="w-80 flex">
               <label>£</label> <input type="text" value={450.0} />
             </div>
-            <input type="checkbox" name="#" id="0" />
+            <input type="checkbox" name="#" id="0" className="" />
             <div>&nbsp;</div>
-            <input type="checkbox" name="#" id="0" />
-            <button>Split</button>
+            <input type="checkbox" name="#" id="0" className="mr-12" />
+            <button
+              onClick={handleAdd}
+              className="bg-blue-600 px-7 py-0.5 text-2xl text-white font-semibold rounded-md shadow-md"
+            >
+              Split
+            </button>
           </div>
         </div>
       </div>
--- src/components/admin/sub_Components/feetable.tsx
+++ src/components/admin/sub_Components/feetable.tsx
@@ -1,3 +1,5 @@
+import { FaRegTrashAlt } from "react-icons/fa";
+
 export function FeeTable() {
   return (
     <div className="datatable">
@@ -5,16 +7,19 @@ export function FeeTable() {
         <input type="text" placeholder="0" />
         <input type="text" placeholder="150,000" />
       </div>
-      <div className="valuesdata">
+      <div className="valuesdata items-center justify-start">
         <div>
           <label>£</label> <input type="text" value={450.0} />
         </div>
-        <input type="checkbox" name="#" id="0" />
+        <input type="checkbox" name="#" id="0" className="" />
         <div>&nbsp;</div>
-        <input type="checkbox" name="#" id="0" />
-        <button>Split</button>
-        <button>Split</button>
-        <button>Delete</button>
+        <input type="checkbox" name="#" id="0" className="w-8" />
+        <div className="flex gap-12">
+          <button className="bg-blue-600 px-7 py-2 text-2xl text-white font-semibold rounded-md shadow-md">
+            Split
+          </button>
+          <FaRegTrashAlt className="w-8 h-8 shadow-md cursor-pointer" />
+        </div>
       </div>
     </div>
   );
