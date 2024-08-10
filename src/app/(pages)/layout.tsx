d47a06979260b4e778cf6470d436eee1e9b7c039
arslan0044 <72161665+arslan0044@users.noreply.github.com>
2024-08-10 13:48:40 +0500

    "Refactor navigation and sidebar components, remove admin navigation, and update layout and styles"

--- src/app/(pages)/layout.tsx
+++ src/app/(pages)/layout.tsx
@@ -1,24 +1,67 @@
 "use client";
-import SideBar from "@/components/admin/SideBar";
-import React, { useState } from "react";
+import Navigation from "@/components/Navigation";
+import SideBar from "@/components/SideBar";
+import React, { useEffect, useState } from "react";
+import axios from 'axios';
+import { useRouter } from 'next/navigation';
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   const [isOpen, setIsOpen] = useState(false);
+  const [isAdmin, setIsAdmin] = useState<boolean>(false); // Added state for isAdmin
+  const [name, setName] = useState<string>(" "); // Added state for isAdmin
+  const [email, setEmail] = useState<string>(" "); // Added state for isAdmin
+  const [id, setId] = useState<number>(10); // Added state for isAdmin
+  const router = useRouter();
+  const handleLogout = async () => {
+    try {
+      const response = await axios.get("/api/auth/logout");
+      if (response.data.success) {
+        alert(response.data.message);
+        router.push("/login");
+      }
+    } catch (error) {
+      console.error("Error logging out:", error);
+    }
+  };
+  useEffect(() => {
+    const fetchAdminStatus = async () => {
+      const response = await axios.get(`/api/user/profile`);
+      setIsAdmin(response.data.data.isAdmin); // Set isAdmin state
+      setName(response.data.data.name); // Set setName state
+      setEmail(response.data.data.email); // Set setEmail state
+      setId(response.data.data.id); // Set setId state
+    };
+    fetchAdminStatus();
+  }, []); // Fetch admin status on component mount
 
   const toggleSidebar = () => {
     setIsOpen(!isOpen);
   };
   return (
     <div>
-      <SideBar isOpen={isOpen} toggleSidebar={toggleSidebar} />
+      <Navigation
+        toggleSidebar={toggleSidebar}
+        isOpen={isOpen}
+        isAdmin={isAdmin}
+        name={name}
+        email={email}
+        id={id}
+        handleLogout={handleLogout}
+      />
+
       <div
-        className={`flex-1 mt-16 p-4 ${
+        className={`flex-1 mt-o p-4 ${
           isOpen ? "ml-[28rem]" : "ml-0"
         } transition-all duration-300 ease-in-out`}
       >
+        <SideBar
+          isOpen={isOpen}
+          isAdmin={isAdmin}
+          handleLogout={handleLogout}
+        />
         {children}
       </div>
     </div>
--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,9 +1,8 @@
 "use client";
-import axios from "axios";
+// import axios from "axios";
 import Image from "next/image";
 import Link from "next/link";
 import React, { useEffect, useState } from "react";
-import { ImEqualizer, ImEqualizer2 } from "react-icons/im";
 import { useRouter } from "next/navigation";
 
 interface MenuItem {
@@ -12,19 +11,12 @@ interface MenuItem {
 }
 interface SidebarProps {
   isOpen: boolean;
-  toggleSidebar: () => void;
+  handleLogout: () => void;
+  isAdmin: boolean
 }
-const SideBar: React.FC<SidebarProps> = ({ isOpen, toggleSidebar }) => {
-  const [isAdmin, setIsAdmin] = useState<boolean>(true); // Added state for isAdmin
+const SideBar: React.FC<SidebarProps> = ({ isOpen, isAdmin, handleLogout }) => {
   const router = useRouter();
-  useEffect(() => {
-    const fetchAdminStatus = async () => {
-      const response = await axios.get(`/api/user/profile`);
-      setIsAdmin(response.data.data.isAdmin); // Set isAdmin state
-    };
-    fetchAdminStatus();
-  }, []); // Fetch admin status on component mount
-
+ 
   const data: MenuItem[] = isAdmin 
     ? [ // Show all links if isAdmin is true
         { link: "/admin", name: "Admin" },
@@ -36,41 +28,25 @@ const SideBar: React.FC<SidebarProps> = ({ isOpen, toggleSidebar }) => {
         { link: "/client", name: "Client" },
       ];
      
-      const handleLogout = async () => {
-        try {
-          const response = await axios.get("/api/auth/logout");
-          if (response.data.success) {
-            alert(response.data.message);
-            router.push("/login");
-          }
-        } catch (error) {
-          console.error("Error logging out:", error);
-        }
-      };
-
+    
 
   return (
     <div
       
       >
-      <button
-        onClick={toggleSidebar}
-        className=" text-black py-2 px-4 font-bold text-6xl rounded-lg fixed top-4 left-4 z-50"
-      >
-        {!isOpen ? <ImEqualizer /> : <ImEqualizer2 />}
-      </button>
+     
       <div
-        className={`fixed top-0 left-0 h-full transition-transform duration-700 ${
+        className={`fixed top-32 left-0 h-full transition-transform duration-700 ${
           isOpen ? "translate-x-0" : "-translate-x-full"
         } w-[28rem] sidebar shadow-lg`}
       >
-        {/* Ensure this div is visible when isOpen is true */}
-        <div className="flex flex-col items-center gap-6 p-4 mt-24">
+       
+        <div className="flex flex-col items-center gap-6 p-4 mt-12">
           <Image
-            src="/assets/profile-icon.png"
+            src={ isAdmin ? "/assets/admin.webp" : "/assets/clientAvatar.webp"}
             width={200}
             height={200}
-            className="rounded-full border border-gray-400"
+            className="rounded-full  border border-gray-400"
             alt="profile"
           />
           <ul className="w-full">
@@ -80,7 +56,7 @@ const SideBar: React.FC<SidebarProps> = ({ isOpen, toggleSidebar }) => {
                 className="w-full flex items-center hover:bg-slate-100"
               >
                 <Link
-                  href={isAdmin ? item.link : "/"} // Updated link logic
+                  href={isAdmin ? item.link : "/client"} 
                   className="block text-2xl font-bold text-center my-2 bg-gray-200 w-full py-2"
                 >
                   {item.name} 
